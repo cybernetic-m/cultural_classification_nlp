cc8b18361aec76cf8b207439566fd1718cb01dc1
Massimo Romano <romano.massimo.uni@gmail.com>
2025-04-08 16:21:00 +0200

    id2string

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,18 @@ def dataset_parser(dataset):
 
     return df, list_id
 
+def id2string(id_, client):
+    # This function transform an ID (both for an entity of wikidata or a pid of a property) to the string
+    # Args: 
+    #       - id: the identifier
+    #       - client: it is the wikidata client that get entity and properties
+
+    element = client.get(id_, load=True)
+    print(element)
+
+
+    
+
+
+    
+
