fc1db184ae917a2f46ed4b69f0a16341a2996784
Massimo Romano <romano.massimo.uni@gmail.com>
2025-04-08 12:21:23 +0200

    dataset_parser

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
-import pandas
+import pandas as pd
 
 
-def dataset_parser():
+def dataset_parser(dataset):
 # Function to read the tsv dataset file and returning it as a pandas dataframe
 # Args: 
+#       - dataset: is the tsv file containing the dataset
 
-    return 0
+    df = pd.read_csv(dataset, sep='\t')
+    print("The dataset is:\n")
+    print(df.head(10))
+    return df
