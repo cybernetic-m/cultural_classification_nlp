59fc91ed6da14344eac463cfbba8490403cb482d
Massimo Romano <romano.massimo.uni@gmail.com>
2025-04-08 14:29:16 +0200

    dataset parser update

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,18 @@ def dataset_parser(dataset):
 # Args: 
 #       - dataset: is the tsv file containing the dataset
 
+    # Read the tsv file using a pandas dataframe
     df = pd.read_csv(dataset, sep='\t')
+
     print("The dataset is:\n")
     print(df.head(10))
+
+    list_id = [] # A list of all wikidata id of the entities
+    urls_col = df['item'] # select the column of each url
+
+    for url in urls_col:
+        print(url)
+
     return df
+
+#df = dataset_parser("./train_set.tsv")
