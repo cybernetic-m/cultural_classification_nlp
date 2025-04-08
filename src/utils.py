db8148e22eccc20d94a4e117d98847a57a577331
Massimo Romano <romano.massimo.uni@gmail.com>
2025-04-08 19:23:14 +0200

    dataset_parser

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,43 @@
 import pandas as pd
 
 
-def dataset_parser(dataset):
+def dataset_parser(dataset, client, list_dict):
 # Function to read the tsv dataset file and returning it as a pandas dataframe
 # Args: 
 #       - dataset: is the tsv file containing the dataset
 
     # Read the tsv file using a pandas dataframe
-    df = pd.read_csv(dataset, sep='\t')
+    original_df = pd.read_csv(dataset, sep='\t')
 
-    list_id = [] # A list of all wikidata id of the entities
-    urls_col = df['item'] # select the column of each url
+    # Dictionary used to map the labels into numbers
+    label_map = {
+    "cultural agnostic": 0,
+    "cultural representative": 1,
+    "cultural exclusive": 2
+    }
+    
+    urls_col = original_df['item'] # select the column of each url
+    label_col = original_df['label'] # select the column of the label
+    name_col = original_df['name'] # select the column of the label
 
     # Taking the url of each data and append to list_id the ID of the wikidata item (Ex. "Q307")
-    for url in urls_col:
-        single_id = url.split("/")[-1] 
-        list_id.append(single_id) 
+    for url, label_ in urls_col, label_col:
+        local_dict = {} # Initialization of a local dict void
+        single_id = url.split("/")[-1] # "Q207"
+        name_id = id2string(single_id, client) # "George W. Bush"
+        entity, prop_list, prop_names_list = getEP(single_id, client)
+        # Creation of the dict referred to the single_id item to append to the list of dictionaries
+        single_item_dict = {
+            single_id:{
+                'name':name_id,
+                'properties': prop_list,
+                'properties_names': prop_names_list,
+                'label': label_map.get(label_, -1)
+                       }
+            }
+        list_dict.append(single_item_dict)
 
-    return df, list_id
+    return original_df, list_dict
 
 def id2string(id_, client):
     # This function transform an ID (both for an entity of wikidata or a pid of a property) to the string
@@ -29,6 +49,24 @@ def id2string(id_, client):
 
     element_ = client.get(id_, load=True)
     return element_.label
+
+def getEP(id_, client):
+    prop_list = []
+    prop_names_list = []
+    entity = client.get(id_, load=True)
+    for prop in entity.data['claims']:
+        prop_list.append(prop)
+        prop_name = id2string(prop, client)
+        prop_names_list.append(prop_name)
+
+    return entity, prop_list, prop_names_list
+
+
+        
+
+
+
+
 
 
     
