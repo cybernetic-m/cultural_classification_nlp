fa62e2aea3e42704cf0e0133ebaa64959df0ecd8
cybernetic-m <romano.massimo.uni@gmail.com>
2025-04-09 12:05:05 +0200

    utils modifications

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,31 @@
 import pandas as pd
+import json 
 
 
-def dataset_parser(dataset, client, list_dict):
-# Function to read the tsv dataset file and returning it as a pandas dataframe
-# Args: 
-#       - dataset: is the tsv file containing the dataset
+def dataset_parser(dataset, client):
+# Function to read the tsv dataset file and returning it as a pandas dataframe (and save it into a json file)
+# Args:
+#       - dataset: it is the tsv file containing the dataset
+#       - client: it is the wikidata client
+# Output:
+#       - original_df: it is the it is the original dataframe
+#       - my_df: 
 
     # Read the tsv file using a pandas dataframe
     original_df = pd.read_csv(dataset, sep='\t')
+    list_dict = [] # Initialization of the empty dictionary
 
     # Dictionary used to map the labels into numbers
     label_map = {
     "cultural agnostic": 0,
+    "cultural ag": 0,
+    "cultural agn": 0,
+    "cultural represent": 1,
     "cultural representative": 1,
+    "cultural ex": 2,
     "cultural exclusive": 2
     }
-    
+
     urls_col = original_df['item'] # select the column of each url
     label_col = original_df['label'] # select the column of the label
     name_col = original_df['name'] # select the column of the label
@@ -30,23 +40,25 @@ def dataset_parser(dataset, client, list_dict):
         single_item_dict = {
             single_id:{
                 'name':name_id,
-                'properties': prop_list,
-                'properties_names': prop_names_list,
-                'label': label_map.get(label_, -1)
+                'properties':{
+                'id': prop_list,
+                'name': prop_names_list,
+                },
+                'label': label_map.get(label_, -1),
                        }
             }
         list_dict.append(single_item_dict)
-        print(single_item_dict)
-
-    return original_df, list_dict
+    
+    dict_save_and_load(list_dict, './entity_properties.json', todo='save')
+    return original_df
 
 def id2string(id_, client):
     # This function transform an ID (both for an entity of wikidata or a pid of a property) to the string
-    # Args: 
+    # Args:
     #       - id: the identifier
     #       - client: it is the wikidata client that get entity and properties
-    # Output: 
-    #       - str that identify the identity or properties 
+    # Output:
+    #       - str that identify the identity or properties
 
     element_ = client.get(id_, load=True)
     return element_.label.get('en')
@@ -62,16 +74,21 @@ def getEP(id_, client):
 
     return entity, prop_list, prop_names_list
 
+def dict_save_and_load(mydict, path, todo):
+  # This function can save or load dictionaries in json format
+  # Args:
+  #       - mydict: it is the python dictionary that you want to save (only needed if todo='save')
+  #       - path: it is the wikidata client that get entity and properties
+  # Output:
+  #       - str that identify the identity or properties
 
-        
+  if todo=="save":
+    # Save it to a JSON file
+    with open(path, "w", encoding="utf-8") as f:
+      json.dump(mydict, f, ensure_ascii=False, indent=4)
 
-
-
-
-
-
-    
-
-
-    
-
+  elif todo=="load":
+    # Load the JSON file
+    with open(path, "r", encoding="utf-8") as f:
+      mydict = json.load(f)
+    return mydict
